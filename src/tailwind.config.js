78bae34ca4e271a7fa762b2aaaa3c68c75e7725a
wendyhamel <whamel@it4web.nl>
2023-01-13 13:13:55 +0100

    styling card responsive and with hover states

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+	theme: {
+		fontFamily : {
+			'sans': ['Outfit', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'cyan': 'hsl(178, 100%, 50%)',
+				'blue-soft': 'hsl(215, 51%, 70%)',
+				'blue': 'hsl(215, 32%, 27%)', // Line
+				'blue-dark': 'hsl(216, 50%, 16%)', // Card BG
+				'blue-darkest': 'hsl(217, 54%, 11%)', // Main BG
+				'white': 'hsl(0, 0%, 100%)',
+			},
+		}
+	}
+}
